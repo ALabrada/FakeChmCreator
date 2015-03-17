0f9582c11784ff6eee2b173919ca9ba5cd52b66c
Alexei Labrada <labrada.alexei@gmail.com>
2015-03-17 10:29:06 -0500

    Created the GUI project.

---
+++ FakeChmCreator.Windows/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FakeChmCreator.Windows.Model;
+
+namespace FakeChmCreator.Windows.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
